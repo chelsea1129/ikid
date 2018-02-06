0c6ea7aec060f9fa35d0a982dd8b68714b609586
Qiaosi Wang <wqs@uw.edu>
2018-02-05 22:39:26 -0800

    working on flip view

---
+++ ikid/ikid/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  ikid
+//
+//  Created by Qiaosi Wang on 2/4/18.
+//  Copyright © 2018 Qiaosi Chelsea Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ikid/ikid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ikid
+//
+//  Created by Qiaosi Wang on 2/4/18.
+//  Copyright © 2018 Qiaosi Chelsea Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
